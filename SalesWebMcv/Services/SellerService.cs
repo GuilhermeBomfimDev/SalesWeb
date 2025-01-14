486f11bfe6d83e5f7998fa6fa991a67ae08a83fe
Guilherme Bomfim <guigui_bsg@hotmail.com>
2025-01-14 18:23:04 -0300

    Update seller and custom service exception

--- SalesWebMcv/Services/SellerService.cs
+++ SalesWebMcv/Services/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using SalesWebMcv.Data;
 using SalesWebMvc.Models;
+using SalesWebMvc.Services.Exceptions;
 using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
@@ -34,6 +35,24 @@ namespace SalesWebMvc.Services
             var obj = _context.Sellers.Find(id);
             _context.Sellers.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Sellers.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                throw new DbConcurrencyException(ex.Message);
+            }
         }
     }
 }
